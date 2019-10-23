a4acceb4f32860c0cabf7ea3125c8776cf3d20dc
tmaraval <maraval.tom@protonmail.com>
2019-10-23 12:10:42 +0200

    working on header parsing

--- nm/inc/ft_nm.h
+++ nm/inc/ft_nm.h
@@ -6,7 +6,7 @@
 /*   By: tmaraval <tmaraval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 15:46:49 by tmaraval          #+#    #+#             */
-/*   Updated: 2019/10/22 16:50:36 by tmaraval         ###   ########.fr       */
+/*   Updated: 2019/10/23 12:04:57 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,25 @@
 # include <fcntl.h>
 # include <sys/mman.h>
 
+# ifdef __linux__
+#	include "../mach_headers/custom.h"
+# else
+#	include <mach-o/loader.h>
+#	include <mach-o/nlist.h>
+# endif
+
 typedef	struct	s_infile
 {
 	void	*mem;
 	size_t	sz;
+	void	*mac_header;
+	void	*load_command;
+	void	*segment_command;
+	void	*symtab_command;
 
 }				t_infile;
 
 t_infile	*process_infile(char *path);
+int			process_header(t_infile *infile);
 
 #endif
--- nm/src/header.c
+++ nm/src/header.c
@@ -1,42 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   header.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmaraval <tmaraval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/22 15:46:47 by tmaraval          #+#    #+#             */
-/*   Updated: 2019/10/23 11:05:44 by tmaraval         ###   ########.fr       */
+/*   Created: 2019/10/23 10:46:29 by tmaraval          #+#    #+#             */
+/*   Updated: 2019/10/23 11:32:36 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_nm.h"
 
-int	process_args(char *file)
+int	error_deref(char *str)
 {
-	t_infile *infile;
-
-	infile = NULL;
-	if (!(infile = process_infile(file)))
-		return (-1);
-	process_header(infile);
-	return (0);
+	ft_putstr_fd("ft_nm : ", 2);
+	ft_putendl_fd(str, 2);
+	return (-1);
 }
 
-int	main(int argc, char **argv)
+int	process_header(t_infile *infile)
 {
-	int i;
+	unsigned long magic_bytes;
 
-	i = 1;
-	if (argc == 1)
-		process_args("./a.out");
-	else
-	{
-		while (i < argc)
-		{
-			process_args(argv[i]);
-			i++;
-		}
-	}
+	magic_bytes = 0;
+	if ((void *)infile->mem + sizeof(unsigned long)
+			> (void *)infile->mem + infile->sz)
+		return (error_deref("corrupted header file"));
+	magic_bytes = (unsigned long)infile->mem + sizeof(unsigned long);
+	printf("magic bytes : %lu", magic_bytes);
 	return (0);
 }
