203796d3e6311e11f48ada67cb1b0c8956012561
tmaraval <maraval.tom@protonmail.com>
2019-10-25 15:33:07 +0200

    check total size of loadcmd

--- nm/src/command.c
+++ nm/src/command.c
@@ -6,7 +6,7 @@
 /*   By: tmaraval <tmaraval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/23 15:55:10 by tmaraval          #+#    #+#             */
-/*   Updated: 2019/10/25 14:48:39 by tmaraval         ###   ########.fr       */
+/*   Updated: 2019/10/25 15:32:11 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,25 +52,47 @@ uint32_t get_load_command_num(t_infile *infile)
 	return (n_lcmds);
 }
 
+int	check_totalsize_load_command(t_infile *file, uint32_t totalsize)
+{
+	if (file->type == IS_32 || file->type == IS_BE)
+	{
+		if (totalsize + sizeof(struct load_command)
+			> ((struct mach_header *)file->mac_header)->sizeofcmds)
+			return (-1);
+	}
+	if (file->type == IS_64 || file->type == IS_BE_64)
+	{
+		if (totalsize + sizeof(struct load_command)
+			> ((struct mach_header_64 *)file->mac_header)->sizeofcmds)
+			return (-1);
+	}
+	return (0);
+}
+
 int	iter_load_command(t_infile *infile)
 {
 	uint32_t		 	n_lcmds;
 	void				*lc;
 	uint32_t			cmdsize;
+	uint32_t			totalsize;
 
 	lc = NULL;
 	n_lcmds = get_load_command_num(infile);
 	lc = (struct load_command *)infile->current;
+	totalsize = 0;
 	while (n_lcmds)
 	{
 		cmdsize = reverse_32(
 			infile->type == IS_BE || infile->type == IS_BE_64,
 			 ((struct load_command *)lc)->cmdsize);
+		if (check_totalsize_load_command(infile, totalsize) < 0)
+			return (error_gen("corrupted load commands"));
 		if (parse_load_command(infile, (struct load_command *)lc) < 0)
 			return (error_gen("corrupted load commands"));
 		if ((void *)lc + sizeof(uint32_t) > (void *)infile->start + infile->sz)
 			return (error_gen("corrupted load commands"));
 		lc = (void *)lc + cmdsize;
+		totalsize += cmdsize;
 		n_lcmds--;
 	}
 	return (0);
