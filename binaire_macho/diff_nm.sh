0b254966f293a77b0d3f8dc8efcef7013475765b
tmaraval <maraval.tom@protonmail.com>
2019-10-30 14:02:00 +0100

    tests

--- binaire_macho/diff_nm.sh
+++ binaire_macho/diff_nm.sh
@@ -2,8 +2,8 @@
 for file in $@ $1
 do
 	echo "diff on $file"
-	../nm_mac $file > resnm 2> errnm
-	../nm/ft_nm $file > resftnm 2> errftnm
+	../nm_mac $file > resnm 2>> errnm
+	../nm/ft_nm $file > resftnm 2>> errftnm
 	diff resnm resftnm
 done
 rm resnm resftnm
