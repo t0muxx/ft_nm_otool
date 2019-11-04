4e42c7c67ccc19e1043f5924a50319c6670020e2
Tom MARAVAL <tmaraval@e1r13p2.42.fr>
2019-11-04 16:28:10 +0100

    norme fat.c ok

--- nm/src/fat.c
+++ nm/src/fat.c
@@ -6,7 +6,7 @@
 /*   By: tmaraval <tmaraval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 15:13:38 by tmaraval          #+#    #+#             */
-/*   Updated: 2019/11/04 16:17:06 by tmaraval         ###   ########.fr       */
+/*   Updated: 2019/11/04 16:27:57 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ int	process_fat_64_work(t_infile *file, void *save_start,
 		struct fat_arch_64 *fat_arch, int i)
 {
 	if ((void *)save_start + reverse_64(1, fat_arch[i].offset)
-	> (void *)save_start + file->sz || (void *)save_start 
-	+ reverse_64(1, fat_arch[i].offset) 
-	+ reverse_64(1,fat_arch[i].size) > (void *)save_start + file->sz )
-		return (error_gen("corrupted fat arch 6"));
+	> (void *)save_start + file->sz || (void *)save_start
+	+ reverse_64(1, fat_arch[i].offset)
+	+ reverse_64(1, fat_arch[i].size) > (void *)save_start + file->sz)
+		return (error_gen("corrupted fat arch"));
 	if (reverse_64(1, fat_arch[i].offset) <= 0)
 		return (error_gen("fat offset <= 0"));
 	file->start = (void *)file->start + reverse_64(1, fat_arch[i].offset);
@@ -39,8 +39,8 @@ int	process_fat_64(t_infile *file)
 	i = 0;
 	if ((void *)file->start + sizeof(struct fat_header)
 	+ sizeof(struct fat_arch_64)
-		> (void *)file->start + file->sz )
-		return (error_gen("corrupted fat header 5 "));
+		> (void *)file->start + file->sz)
+		return (error_gen("corrupted fat header"));
 	fat_arch = file->start + sizeof(struct fat_header);
 	fat_head = file->start;
 	while (i < reverse_32(1, fat_head->nfat_arch))
@@ -50,20 +50,20 @@ int	process_fat_64(t_infile *file)
 		file->start = save_start;
 		if ((void *)fat_arch + sizeof(struct fat_arch_64)
 		> (void *)file->save + file->sz)
-			return (error_gen("corrupted fat arch 7"));
+			return (error_gen("corrupted fat arch"));
 		i++;
 	}
 	return (0);
 }
 
 int	process_fat_32_work(t_infile *file, void *save_start,
-	   struct fat_arch *fat_arch, int i)
+			struct fat_arch *fat_arch, int i)
 {
 	if ((void *)save_start + reverse_32(1, fat_arch[i].offset)
-	> (void *)save_start + file->sz || (void *)save_start 
-	+ reverse_32(1, fat_arch[i].offset) 
-	+ reverse_32(1,fat_arch[i].size) > (void *)save_start + file->sz )
-		return (error_gen("corrupted fat arch 4"));
+	> (void *)save_start + file->sz || (void *)save_start
+	+ reverse_32(1, fat_arch[i].offset)
+	+ reverse_32(1, fat_arch[i].size) > (void *)save_start + file->sz)
+		return (error_gen("corrupted fat arch"));
 	if (reverse_32(1, fat_arch[i].offset) <= 0)
 		return (error_gen("fat offset <= 0"));
 	file->start = (void *)file->start + reverse_32(1, fat_arch[i].offset);
@@ -93,7 +93,7 @@ int	process_fat_32(t_infile *file)
 		file->start = save_start;
 		if ((void *)fat_arch + sizeof(struct fat_arch)
 		> (void *)file->save + file->sz)
-			return (error_gen("corrupted fat arch 5"));
+			return (error_gen("corrupted fat arch"));
 		i++;
 	}
 	return (0);
@@ -105,14 +105,14 @@ int	process_fat(t_infile *file)
 
 	magic_bytes = 0;
 	if (protect(file, (void *)file->start + sizeof(unsigned long)) < 0)
-		return (error_gen("corrupted fat header 0"));
+		return (error_gen("corrupted fat header"));
 	magic_bytes = *(uint32_t *)file->start;
 	if (magic_bytes != FAT_MAGIC && magic_bytes != FAT_MAGIC_64
 	&& magic_bytes != FAT_CIGAM && magic_bytes != FAT_CIGAM_64)
 		return (1);
 	if (protect(file, (void *)file->start
 	+ sizeof(struct fat_header) + sizeof(cpu_type_t)) < 0)
-		return (error_gen("corrupted fat header 1"));
+		return (error_gen("corrupted fat header"));
 	if (search_cputype_x64(file, magic_bytes) > 0)
 		return (process_macho(file));
 	if (magic_bytes == FAT_MAGIC || magic_bytes == FAT_CIGAM)
