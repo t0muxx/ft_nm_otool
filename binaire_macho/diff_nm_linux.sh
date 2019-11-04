550c0796200b9ac52e399a96cfa587edf5e35d2a
tmaraval <maraval.tom@protonmail.com>
2019-11-04 18:20:10 +0100

    file to tes

--- binaire_macho/diff_nm_linux.sh
+++ binaire_macho/diff_nm_linux.sh
@@ -3,12 +3,14 @@ rm errftnm errnm
 for file in $@ $1
 do
 	echo "diff on $file"
-	if [ -s "$file.err" ]
+	dir=${file%/*}
+	filename=${file##*/}
+	if [ -s "$dir/nm_out/$filename.err" ]
 	then
 		echo "err by nm"
 	else
 		echo $file >> errftnm
-		cat $file.out > resnm 2>> errnm
+		cat $dir/nm_out/$filename.out > resnm 2>> errnm
 		../nm/ft_nm $file > resftnm 2>> errftnm
 		diff resnm resftnm
 	fi
