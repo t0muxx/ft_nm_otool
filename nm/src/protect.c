804da8b6539484cdd5ff4b912c15c4b6b95dd876
Tom MARAVAL <tmaraval@e1r13p2.42.fr>
2019-11-04 16:39:15 +0100

    norme ok protect.c

--- nm/src/protect.c
+++ nm/src/protect.c
@@ -6,13 +6,13 @@
 /*   By: tmaraval <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 13:16:19 by tmaraval          #+#    #+#             */
-/*   Updated: 2019/11/04 13:42:21 by tmaraval         ###   ########.fr       */
+/*   Updated: 2019/11/04 16:39:07 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_nm.h"
 
-int protect(t_infile *file, void *ptr)
+int	protect(t_infile *file, void *ptr)
 {
 	if (ptr > (void *)file->save + file->sz)
 	{
