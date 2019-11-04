91e362e7212db1dae68ed73248336c476ac62ded
Tom MARAVAL <tmaraval@e1r13p2.42.fr>
2019-11-04 15:31:23 +0100

    refactor archive.c ok

--- nm/src/archive.c
+++ nm/src/archive.c
@@ -6,7 +6,7 @@
 /*   By: tmaraval <tmaraval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/03 17:41:54 by tmaraval          #+#    #+#             */
-/*   Updated: 2019/11/04 15:23:34 by tmaraval         ###   ########.fr       */
+/*   Updated: 2019/11/04 15:30:04 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ void	archive_fw_symdef(t_infile *file, struct ar_hdr *ar_header)
 	{
 		name = (char *)ar_header + sizeof(struct ar_hdr);
 		ret = ft_atoi((char *)ar_header->ar_name + ft_strlen(AR_EFMT1));
-		if (!ft_strncmp(name, SYMDEF, ret) || !ft_strncmp(name, SYMDEF_SORTED, ret))
+		if (!ft_strncmp(name, SYMDEF, ret) ||
+		!ft_strncmp(name, SYMDEF_SORTED, ret))
 		{
 			file->start = (void *)file->start +
 				sizeof(struct ar_hdr) + ft_atoi(ar_header->ar_size);
@@ -31,34 +32,45 @@ void	archive_fw_symdef(t_infile *file, struct ar_hdr *ar_header)
 	}
 }
 
+int	parse_archive_member_work(t_infile *file, struct ar_hdr *ar_header)
+{
+	unsigned long magic_bytes;
+	int		offset;
+	
+	offset = 0;
+	magic_bytes = 0;
+	ar_header = file->start;
+	if (ft_strncmp(ar_header->ar_fmag, "`\n", 2))
+		return (error_gen("malformed archive header"));
+	offset = get_str_offset_archive(ar_header);
+	file->start = (void *)file->start + sizeof(struct ar_hdr) + offset;
+	magic_bytes = *(uint32_t *)((void *)file->start);
+	if (!(!ft_strncmp((char *)file->start, ARMAG, SARMAG)
+	|| magic_bytes == FAT_MAGIC || magic_bytes == FAT_MAGIC_64 || 
+	magic_bytes == FAT_CIGAM || magic_bytes == FAT_CIGAM_64))
+	{
+		print_archive_member(file, ar_header);
+		if (process_macho(file) < 0)
+			return (-1);
+	}
+	file->start = (void *)file->start + ft_atoi(ar_header->ar_size) - offset;
+	offset = get_str_offset_archive((struct ar_hdr *)((void *)file->start
+				+ sizeof(struct ar_hdr)));
+	return (0);
+}
+
 int	parse_archive_member(t_infile *file, struct ar_hdr *ar_header)
 {
-	int		offset;
-	unsigned long magic_bytes;
 
-	magic_bytes = 0;
-	offset = 0;
-	if ((void *)ar_header + sizeof(struct ar_hdr) + ft_atoi(ar_header->ar_size)
-		> (void *)file->start + file->sz)
+	if (protect(file, (void *)ar_header + sizeof(struct ar_hdr)
+	+ ft_atoi(ar_header->ar_size)) < 0)
 		return (-1);
 	file->start = (void *)file->start + SARMAG;
 	archive_fw_symdef(file, (struct ar_hdr *)file->start);
 	while ((void *)file->start < (void *)file->save + file->ar_sz)
 	{
-		ar_header = file->start;
-		if (ft_strncmp(ar_header->ar_fmag, "`\n", 2))
-			return (error_gen("malformed archive header"));
-		offset = get_str_offset_archive(ar_header);
-		file->start = (void *)file->start + sizeof(struct ar_hdr) + offset;
-		magic_bytes = *(uint32_t *)((void *)file->start);
-		if (!(!ft_strncmp((char *)file->start, ARMAG, SARMAG) || magic_bytes == FAT_MAGIC || magic_bytes == FAT_MAGIC_64 || magic_bytes == FAT_CIGAM || magic_bytes == FAT_CIGAM_64))
-		{
-			print_archive_member(file, ar_header);
-			if (process_macho(file) < 0)
-				return (-1);
-		}
-			file->start = (void *)file->start + ft_atoi(ar_header->ar_size) - offset;
-			offset = get_str_offset_archive((struct ar_hdr *)((void *)file->start + sizeof(struct ar_hdr)));
+		if (parse_archive_member_work(file, ar_header) < 0)
+			return (-1);
 	}
 	return (0);
 }
