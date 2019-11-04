21b7f937eb3b17b09f5db998b0e67cbb63fb0767
Tom MARAVAL <tmaraval@e1r13p2.42.fr>
2019-11-04 16:30:36 +0100

    norme ok fat_utils.c

--- nm/src/fat_utils.c
+++ nm/src/fat_utils.c
@@ -6,7 +6,7 @@
 /*   By: tmaraval <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 15:48:28 by tmaraval          #+#    #+#             */
-/*   Updated: 2019/11/04 16:17:20 by tmaraval         ###   ########.fr       */
+/*   Updated: 2019/11/04 16:30:24 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,8 @@ void	fat_print_arch(t_infile *file, cpu_type_t cputype,
 	}
 }
 
-int	search_cputype_x64_work_off_ar_sz(t_infile *file, unsigned long magic_bytes,
-		void *fat_arch, uint64_t *offset_arsz)
+int		search_cputype_x64_work_off_ar_sz(t_infile *file,
+		unsigned long magic_bytes, void *fat_arch, uint64_t *offset_arsz)
 {
 	if (magic_bytes == FAT_MAGIC || magic_bytes == FAT_CIGAM)
 	{
@@ -48,25 +48,27 @@ int	search_cputype_x64_work_off_ar_sz(t_infile *file, unsigned long magic_bytes,
 	{
 		if (protect(file, (void *)fat_arch + sizeof(struct fat_arch_64)) < 0)
 			return (error_gen("corrupted far_arch"));
-		offset_arsz[0] = reverse_64(1, ((struct fat_arch_64 *)fat_arch)->offset);
+		offset_arsz[0] = reverse_64(1,
+				((struct fat_arch_64 *)fat_arch)->offset);
 		offset_arsz[1] = reverse_64(1, ((struct fat_arch_64 *)fat_arch)->size);
 	}
 	return (0);
 }
 
-int	search_cputype_x64_work(t_infile *file, unsigned long magic_bytes,
+int		search_cputype_x64_work(t_infile *file, unsigned long magic_bytes,
 		void *fat_arch)
 {
 	uint64_t			offset_arsz[2];
-	
-	offset_arsz[0] = 0;	
+
+	offset_arsz[0] = 0;
 	offset_arsz[1] = 0;
 	if (search_cputype_x64_work_off_ar_sz(file, magic_bytes,
 	fat_arch, offset_arsz) < 0)
-		return (-1);	
+		return (-1);
 	if (offset_arsz[0] <= 0)
 		return (error_gen("fat offset <= 0"));
-	if (reverse_32(1, ((struct fat_arch *)fat_arch)->cputype) == CPU_TYPE_X86_64)
+	if (reverse_32(1,
+			((struct fat_arch *)fat_arch)->cputype) == CPU_TYPE_X86_64)
 	{
 		file->ar_sz = offset_arsz[1];
 		file->start = (void *)file->start + offset_arsz[0];
@@ -75,8 +77,7 @@ int	search_cputype_x64_work(t_infile *file, unsigned long magic_bytes,
 	return (0);
 }
 
-
-int	search_cputype_x64(t_infile *file, unsigned long magic_bytes)
+int		search_cputype_x64(t_infile *file, unsigned long magic_bytes)
 {
 	struct fat_header	*fat_header;
 	uint32_t			i;
