dcd955e3a2a69a29a49ccfe2cae79cf5b17a7e10
Tom MARAVAL <tmaraval@e1r13p2.42.fr>
2019-11-04 16:53:13 +0100

    norme error.c

--- nm/src/error.c
+++ nm/src/error.c
@@ -6,15 +6,25 @@
 /*   By: tmaraval <tmaraval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/24 09:48:56 by tmaraval          #+#    #+#             */
-/*   Updated: 2019/10/24 09:49:20 by tmaraval         ###   ########.fr       */
+/*   Updated: 2019/11/04 16:49:02 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_nm.h"
 
-int	error_gen(char *str)
+int		error_gen(char *str)
 {
 	ft_putstr_fd("ft_nm : ", 2);
 	ft_putendl_fd(str, 2);
 	return (-1);
+}
+
+size_t	protected_strlen(char *str, t_infile *file)
+{
+	size_t i;
+
+	i = 0;
+	while (((void *)str + i < (void *)file->save + file->sz) && str[i])
+		i++;
+	return (i);
 }
