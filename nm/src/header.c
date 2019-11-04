78a2f7a169a4e913e4eb88c0a512845515a29ea3
Tom MARAVAL <tmaraval@e1r13p2.42.fr>
2019-11-04 16:32:02 +0100

    norme ok header.c

--- nm/src/header.c
+++ nm/src/header.c
@@ -6,7 +6,7 @@
 /*   By: tmaraval <tmaraval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/23 10:46:29 by tmaraval          #+#    #+#             */
-/*   Updated: 2019/11/04 14:36:03 by tmaraval         ###   ########.fr       */
+/*   Updated: 2019/11/04 16:31:54 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	process_header_full(t_infile *infile)
 {
-	if (infile->type == IS_32 || infile->type == IS_BE )
+	if (infile->type == IS_32 || infile->type == IS_BE)
 	{
 		if (protect(infile, (void *)infile->start
 		+ sizeof(struct mach_header)) < 0)
@@ -42,10 +42,9 @@ int	process_header(t_infile *infile)
 	if (protect(infile, (void *)infile->start + sizeof(uint32_t)) < 0)
 		return (error_gen("corrupted header file"));
 	magic_bytes = *(uint32_t *)infile->start;
-//	printf("--> %lx\n", magic_bytes);
 	if (magic_bytes == MH_MAGIC)
 		infile->type = IS_32;
-	else if (magic_bytes  == MH_MAGIC_64)
+	else if (magic_bytes == MH_MAGIC_64)
 		infile->type = IS_64;
 	else if (magic_bytes == MH_CIGAM)
 		infile->type = IS_BE;
@@ -53,9 +52,5 @@ int	process_header(t_infile *infile)
 		infile->type = IS_BE_64;
 	else
 		return (-1);
-#ifdef DEBUG_HEADER
-	ft_printf("|DEBUG| -> type = %d\n", infile->type);
-#endif
 	return (process_header_full(infile));
 }
-
